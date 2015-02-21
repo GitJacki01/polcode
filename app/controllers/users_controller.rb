d7ed16d7640941c5d8fe6041661cd703b6c8bdbb
Andrzej Jackiewicz <git01@sokolka.com>
2015-02-21 18:07:03 +0100

    Task 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def show
     @user = User.find(params[:id])
     unless @user == current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'all_users', to: 'users#index'
   devise_for :users
   resources :users
 end

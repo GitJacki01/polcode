39497ac5baa4dbeb93d3b42952ff4c8fe2b65b87
Andrzej Jackiewicz <git01@sokolka.com>
2015-02-25 18:50:50 +0100

    Task 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+#user = CreateAdminService.new.call
+#puts 'CREATED ADMIN USER: ' << user.email
+
+City.create([
+  [{name: 'Warszawa', short_name: 'W-wa'}],
+  [{name: 'Kraków', short_name: 'Krk'}],
+  [{name: 'Białystok', short_name: 'B-stok'}],
+  [{name: 'Sokółka', short_name: 'S-ka'}]
+])
